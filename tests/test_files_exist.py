7e1ea6e67b062073ff7ac3dd0fcd25e268fd7888
Daniel Ruiz <43095611+thegitofdaniel@users.noreply.github.com>
2024-01-12 09:05:50 +0000

    Add unit tests

--- tests/test_files_exist.py
+++ tests/test_files_exist.py
@@ -1,5 +1,5 @@
 import os
 
 def test_index_html_exists():
-    file_name = 'index.html'
+    file_name = '/workspaces/thewebsiteofdaniel/index.html'
     assert os.path.isfile(file_name), f"{file_name} does not exist"
